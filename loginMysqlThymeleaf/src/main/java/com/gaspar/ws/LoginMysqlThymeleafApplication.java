ab7d6e9e7a26fecf092a81c84a7dbd4d74327f3c
Gaspar <e972136@gmail.com>
2022-08-10 16:08:41 -0600

    inicio login

---
+++ loginMysqlThymeleaf/src/main/java/com/gaspar/ws/LoginMysqlThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMysqlThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMysqlThymeleafApplication.class, args);
+	}
+
+}
---
+++ loginMysqlThymeleaf/src/test/java/com/gaspar/ws/LoginMysqlThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspar.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMysqlThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
